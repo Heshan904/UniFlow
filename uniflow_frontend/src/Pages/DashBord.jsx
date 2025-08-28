a8634aabc08c71ae1b6d9a2af23eaf88a485f14a
Heshan904 <heshanhasitha904@gmail.com>
2025-08-28 12:53:11 +0530

    make

--- uniflow_frontend/src/Pages/DashBord.jsx
+++ uniflow_frontend/src/Pages/DashBord.jsx
@@ -7,12 +7,14 @@ import './dashbord.css'
 
 
 
+
 const noticeImages = [
   "https://res.cloudinary.com/da2wbtci0/image/upload/v1755964000/WhatsApp_Image_2025-08-22_at_12.53.38_PM_j2h0ua.jpg",
   "https://res.cloudinary.com/da2wbtci0/image/upload/v1755964001/WhatsApp_Image_2025-08-21_at_12.26.17_PM_fjchvn.jpg",
   "https://res.cloudinary.com/da2wbtci0/image/upload/v1755964001/WhatsApp_Image_2025-08-22_at_7.52.54_AM_rgaftc.jpg"
 ];
 function DashBord() {
+
   return (
     <>
     <NaviBar/>
